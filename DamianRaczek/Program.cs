c7ba92ce2502a897c7b217d5f8853cf6c33d3222
Damian Raczek <draczekProjekt@gmail.com>
2024-10-28 12:21:54 +0100

    Function for reading albums from file implemented.

--- DamianRaczek/Program.cs
+++ DamianRaczek/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DamianRaczek
+﻿using System.Data;
+
+namespace DamianRaczek
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            var x = ReadAlbumsFromFile();
         }
+
+        public static List<Music> ReadAlbumsFromFile()
+        {
+            List<Music> list = new List<Music>();
+            StreamReader streamReader = new StreamReader("Data.txt");
+            while(!streamReader.EndOfStream)
+            {
+                Music music = new Music()
+                {
+                    Artist = streamReader.ReadLine() ?? "",
+                    Album = streamReader.ReadLine() ?? "",
+                    SongsNumber = int.Parse(streamReader.ReadLine() ?? ""),
+                    Year = int.Parse(streamReader.ReadLine() ?? ""),
+                    DownloadNumber = int.Parse(streamReader.ReadLine() ?? "")
+                };
+                streamReader.ReadLine();
+                list.Add(music);
+            }
+
+            return list;
+        }
+
+
     }
 }
